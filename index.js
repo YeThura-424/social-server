ed889f41b16bf0bc1faf700b087352070ff9e501
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-31 23:06:32 +0630

    mongoose setup

--- index.js
+++ index.js
@@ -35,3 +35,15 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// mongoose setup
+const PORT = process.env.PORT || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+}).catch((error) => {
+  console.log(error);
+});
