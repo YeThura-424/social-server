22b277fd391812791eef95ba77a28b957b1eff19
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-01 23:09:15 +0630

    user routes and controller for get, friends and addremovefriends

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from "./routes/auth.js";
+import userRoutes from './routes/users.js';
 import { register } from "./controllers/auth.js";
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -42,6 +43,8 @@ app.post("auth/register", upload.single("picture"), register);
 
 //Routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
 // mongoose setup
 const PORT = process.env.PORT || 6001;
 
