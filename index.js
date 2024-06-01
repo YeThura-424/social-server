c6de0de3d784ff306fe5be1975b600c44b154724
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-01 21:58:55 +0630

    routes for auth

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import { register } from "./controllers/auth.js";
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,6 +35,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//Routes with files
+app.post("auth/register", upload.single("picture"), register);
 
 // mongoose setup
 const PORT = process.env.PORT || 6001;
