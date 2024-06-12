24bf0ee991368aac18613f37d5858c56cf2dc269
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-12 19:57:07 +0630

    update public assets file path

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import multer from 'multer';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from './routes/users.js';
+import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { createPost } from './controllers/posts.js';
-import { verifyToken } from './middleware/auth.js';
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,7 +26,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "pupbli/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // File Storage
 
@@ -52,11 +52,14 @@ app.use("/posts", postRoutes);
 // mongoose setup
 const PORT = process.env.PORT || 6001;
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-}).catch((error) => {
-  console.log(error);
-});
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
