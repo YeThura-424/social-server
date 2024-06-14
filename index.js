8828f9927643080aa274c2aeef2a84d12f65390d
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-14 22:13:24 +0630

    wip

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -63,9 +63,6 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => {
     console.log(error);
